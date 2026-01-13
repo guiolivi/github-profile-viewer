0a3afaf312d35ff9b4207f15fae4f2510bad171b
guiolivi <guilhermeoli.dev@gmail.com>
2026-01-12 23:55:18 -0300

    fix: clean input after page reload

--- src/script.js
+++ src/script.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const input = document.querySelector("#search-input");
 const placeholder = document.querySelector(".placeholder");
 
+document.addEventListener("DOMContentLoaded", () => {
+    input.value = "";
+})
+
 input.addEventListener("input", () => {
     placeholder.style.display = input.value ? "none" : "block";
 })
