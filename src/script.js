a9c6582de6b46520879de3438cfeb35a014ac7a1
guiolivi <guilhermeoli.dev@gmail.com>
2026-01-12 23:37:23 -0300

    feat: finish styling header, info panel and search bar

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
 import axios from 'axios';
+
+const input = document.querySelector("#search-input");
+const placeholder = document.querySelector(".placeholder");
+
+input.addEventListener("input", () => {
+    placeholder.style.display = input.value ? "none" : "block";
+})
