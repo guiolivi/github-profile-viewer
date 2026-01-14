97a631cd91c2939faadda156caaba2fa32922f27
guiolivi <guilhermeoli.dev@gmail.com>
2026-01-13 21:00:27 -0300

    feat: finish styling main elements

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const input = document.querySelector("#search-input");
 const placeholder = document.querySelector(".placeholder");
+const teste = document.querySelector("#theme-toggle");
 
 document.addEventListener("DOMContentLoaded", () => {
     input.value = "";
